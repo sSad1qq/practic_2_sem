afe75d94843c7f8d52e9915a8941917064e53931
sSad1qq <danil.dyatlov@icloud.com>
2026-06-25 13:10:23 +0300

    upd

---
+++ build/CMakeFiles/repos.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task1/main.cpp" "CMakeFiles/repos.dir/task1/main.cpp.o" "gcc" "CMakeFiles/repos.dir/task1/main.cpp.o.d"
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task1/mod.cpp" "CMakeFiles/repos.dir/task1/mod.cpp.o" "gcc" "CMakeFiles/repos.dir/task1/mod.cpp.o.d"
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task2/main.cpp" "CMakeFiles/repos.dir/task2/main.cpp.o" "gcc" "CMakeFiles/repos.dir/task2/main.cpp.o.d"
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task2/mod.cpp" "CMakeFiles/repos.dir/task2/mod.cpp.o" "gcc" "CMakeFiles/repos.dir/task2/mod.cpp.o.d"
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task3/main.cpp" "CMakeFiles/repos.dir/task3/main.cpp.o" "gcc" "CMakeFiles/repos.dir/task3/main.cpp.o.d"
+  "/Users/daniildyatlov/develop/git/prac/practic_2_sem/task3/mod.cpp" "CMakeFiles/repos.dir/task3/mod.cpp.o" "gcc" "CMakeFiles/repos.dir/task3/mod.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/repos.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/repos.dir/task1/main.cpp.o"
+  "CMakeFiles/repos.dir/task1/main.cpp.o.d"
+  "CMakeFiles/repos.dir/task1/mod.cpp.o"
+  "CMakeFiles/repos.dir/task1/mod.cpp.o.d"
+  "CMakeFiles/repos.dir/task2/main.cpp.o"
+  "CMakeFiles/repos.dir/task2/main.cpp.o.d"
+  "CMakeFiles/repos.dir/task2/mod.cpp.o"
+  "CMakeFiles/repos.dir/task2/mod.cpp.o.d"
+  "CMakeFiles/repos.dir/task3/main.cpp.o"
+  "CMakeFiles/repos.dir/task3/main.cpp.o.d"
+  "CMakeFiles/repos.dir/task3/mod.cpp.o"
+  "CMakeFiles/repos.dir/task3/mod.cpp.o.d"
+  "repos"
+  "repos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/repos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
